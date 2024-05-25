b13578c4b7607fa6ce562ce171075ce032ebdec7
stron <yaneliyahu83@gmail.com>
2024-05-25 21:25:51 +0300

    feat(types): add measurement types

---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type MeasuringUnit = "imperial" | "metric";
+
+export type Measurement = "height" | "weight";
+
+export type HeightMeasurements = "cm" | "ft" | "in";
+
+export type WeightMeasurements = "st" | "lbs" | "kg";
