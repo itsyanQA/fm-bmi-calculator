00fc78948a90852b22e33dcc7bfe47dc8e3b4d4a
stron <yaneliyahu83@gmail.com>
2024-05-27 21:13:10 +0300

    refactor(bmi-calculation): make params objects and add typing

--- src/utils/bmi-calculation.ts
+++ src/utils/bmi-calculation.ts
@@ -1,21 +1,33 @@
-export function calculateBmiImperial(feet: number, inches: number, stones: number, pounds: number) {
+export type ImperialCalculationParams = {
+  ft: string;
+  inches: string;
+  st: string;
+  lbs: string;
+};
+
+export type MetricCalculationParams = {
+  kg: string;
+  cm: string;
+};
+
+export function calculateBmiImperial(imperialParams: ImperialCalculationParams) {
   // Convert height to inches
-  const heightInches = feet * 12 + inches;
+  const heightInches = +imperialParams.ft * 12 + imperialParams.inches;
   // Convert height to meters
-  const heightMeters = heightInches * 0.0254;
+  const heightMeters = +heightInches * 0.0254;
 
   // Convert weight to pounds
-  const weightPounds = stones * 14 + pounds;
+  const weightPounds = +imperialParams.st * 14 + imperialParams.lbs;
   // Convert weight to kilograms
-  const weightKilograms = weightPounds * 0.453592;
+  const weightKilograms = +weightPounds * 0.453592;
 
   // Calculate BMI
   const bmi = weightKilograms / (heightMeters * heightMeters);
-  return bmi;
+  return bmi.toFixed(1);
 }
 
-export function calculateBmiMetric(weight: number, heightCm: number) {
+export function calculateBmiMetric(metricParams: MetricCalculationParams) {
   // Convert height from cm to m
-  const height = heightCm / 100;
-  return weight / (height * height);
+  const height = +metricParams.cm / 100;
+  return (+metricParams.kg / (height * height)).toFixed(1);
 }
