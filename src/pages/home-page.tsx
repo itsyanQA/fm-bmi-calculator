036f2a38c234aaf766b9e8ead1e94ffabd3b6f72
Yan Eliyahu <yan@adom.technology>
2024-06-07 16:15:44 +0300

    style: align header and main to match design

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,10 +1,14 @@
-import { Header } from "@components/ui/header";
 import "./home-page.scss";
+import { Header } from "@components/ui/header";
+import { BmiResultMeaning } from "@features/bmi-result-meaning";
+import { RecommendationsList } from "@features/recommendations";
 
 export function HomePage() {
   return (
-    <>
+    <main>
       <Header />
-    </>
+      <BmiResultMeaning />
+      <RecommendationsList />
+    </main>
   );
 }
