b1e6254adeefb7274e352f7e57e0403f8c18766d
Yan Eliyahu <yan@adom.technology>
2024-06-07 16:14:39 +0300

    feat: create data.ts file

---
+++ src/data/data.ts
@@ -0,0 +1,24 @@
+import SleepIcon from "@assets/icon-sleep.svg?react";
+import ExerciseIcon from "@assets/icon-exercise.svg?react";
+import EatingIcon from "@assets/icon-eating.svg?react";
+
+export const RECOMMENDATIONS = [
+  {
+    icon: SleepIcon,
+    title: "Adequate sleep",
+    description:
+      "Sleep enhances mental clarity, emotional stability, and physical wellness, promoting overall restoration and rejuvenation.",
+  },
+  {
+    icon: ExerciseIcon,
+    title: "Regular exercise",
+    description:
+      "Exercise improves fitness, aids weight control, elevates mood, and reduces disease risk, fostering wellness and longevity.",
+  },
+  {
+    icon: EatingIcon,
+    title: "Healthy eating",
+    description:
+      "Healthy eating promotes weight control, disease prevention, better digestion, immunity, mental clarity, and mood.",
+  },
+];
