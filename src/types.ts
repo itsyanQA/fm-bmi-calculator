bba883a109117dc9cb676e028b90d93612f59ffd
stron <yaneliyahu83@gmail.com>
2024-07-06 19:26:52 +0300

    chore(types): add Limitation and WeightStatus types

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,7 @@ export type Measurement = "height" | "weight";
 export type HeightMeasurements = "cm" | "ft" | "in";
 
 export type WeightMeasurements = "st" | "lbs" | "kg";
+
+export type Limitation = "gender" | "age" | "muscle" | "pregnancy" | "race";
+
+export type WeightStatus = "underweight" | "healthy-weight" | "overweight" | "obese";
